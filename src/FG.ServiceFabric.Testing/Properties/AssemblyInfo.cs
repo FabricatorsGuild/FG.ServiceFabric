519c15797ade6752280ed3b94a364e1d6329c965
rnilsark <richard.nilsark@ffcg.se>
2017-07-07 08:31:40 +0200

    Use common version.

--- src/FG.ServiceFabric.Testing/Properties/AssemblyInfo.cs
+++ src/FG.ServiceFabric.Testing/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2f2b5701-77b5-4467-9504-8be73a3725a7")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
