d2f7808fef59fecdabc49d8ee58c1abb4e62789f
yoape <fredrik.goransson@gmail.com>
2018-01-23 16:26:20 +0100

    Renamed perstance tests to separate between providers

--- src/FG.ServiceFabric.Tests.Persistence/Services/Runtime/With_StateSession_DocumentDbStateSession.cs
+++ src/FG.ServiceFabric.Tests.Persistence/Services/Runtime/With_StateSession_DocumentDbStateSession.cs
@@ -9,153 +9,165 @@ using NUnit.Framework;
 
 namespace FG.ServiceFabric.Tests.Persistence.Services.Runtime
 {
-    public class With_StateSession_DocumentDbStateSession : With_StateSession_All_Tests
+    namespace With_StateSession_DocumentDbStateSession
     {
-        private class TestRunnerBase<T> : With_StateSession_All_Tests.TestRunnerWithFunc<T> where T : StatefulServiceDemoBase
+        public class
+            With_StateSession_All_Tests : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests
         {
-            private readonly Guid _appId = Guid.NewGuid();
-            private string _collectionName;
-            private CosmosDbForTestingSettingsProvider _cosmosDbSettingsProvider;
-            private DocumentDbStateSessionManagerWithTransactions _stateSessionManager;
-
-            public override IDictionary<string, string> State => GetState();
-
-            public override IStateSessionManager CreateStateManager(MockFabricRuntime fabricRuntime,
-                StatefulServiceContext context)
+            private class TestRunnerBase<T> : With_StateSession_All_Tests.TestRunnerWithFunc<T>
+                where T : StatefulServiceDemoBase
             {
-                _collectionName = $"App-tests-{_appId}";
+                private readonly Guid _appId = Guid.NewGuid();
+                private string _collectionName;
+                private CosmosDbForTestingSettingsProvider _cosmosDbSettingsProvider;
+                private DocumentDbStateSessionManagerWithTransactions _stateSessionManager;
 
-                _cosmosDbSettingsProvider =
-                    CosmosDbForTestingSettingsProvider.DefaultForCollection(_collectionName);
-                _stateSessionManager = new DocumentDbStateSessionManagerWithTransactions(
-                    StateSessionHelper.GetServiceName(context.ServiceName),
-                    _appId,
-                    StateSessionHelper.GetPartitionInfo(context, () => fabricRuntime.PartitionEnumerationManager)
-                        .GetAwaiter()
-                        .GetResult(),
-                    _cosmosDbSettingsProvider
-                );
+                public override IDictionary<string, string> State => GetState();
 
-                return _stateSessionManager;
+                public override IStateSessionManager CreateStateManager(MockFabricRuntime fabricRuntime,
+                    StatefulServiceContext context)
+                {
+                    _collectionName = $"App-tests-{_appId}";
+
+                    _cosmosDbSettingsProvider =
+                        CosmosDbForTestingSettingsProvider.DefaultForCollection(_collectionName);
+                    _stateSessionManager = new DocumentDbStateSessionManagerWithTransactions(
+                        StateSessionHelper.GetServiceName(context.ServiceName),
+                        _appId,
+                        StateSessionHelper.GetPartitionInfo(context, () => fabricRuntime.PartitionEnumerationManager)
+                            .GetAwaiter()
+                            .GetResult(),
+                        _cosmosDbSettingsProvider
+                    );
+
+                    return _stateSessionManager;
+                }
+
+                private IDictionary<string, string> GetState()
+                {
+                    if (_stateSessionManager is IDocumentDbDataManager dataManager)
+                        return dataManager.GetCollectionDataAsync(_collectionName).GetAwaiter().GetResult();
+                    return new Dictionary<string, string>();
+                }
+
+                private void DestroyCollection()
+                {
+                    if (_stateSessionManager is IDocumentDbDataManager dataManager)
+                        dataManager.DestroyCollecton(_collectionName).GetAwaiter().GetResult();
+                }
+
+                protected override void OnTearDown()
+                {
+                    base.OnTearDown();
+
+                    DestroyCollection();
+                }
+
+                public TestRunnerBase(Func<StatefulServiceContext, IStateSessionManager, T> createService) : base(
+                    createService)
+                {
+                }
             }
 
-            private IDictionary<string, string> GetState()
+            public class StateSession_transacted_scope : Runtime.StateSession_transacted_scope
             {
-                if (_stateSessionManager is IDocumentDbDataManager dataManager)
-                    return dataManager.GetCollectionDataAsync(_collectionName).GetAwaiter().GetResult();
-                return new Dictionary<string, string>();
+                private string _sessionId;
+                private CosmosDbForTestingSettingsProvider _settingsProvider;
+                private DocumentDbStateSessionManagerWithTransactions _stateSessionManager;
+
+                protected override IStateSessionManager GetStateSessionManager()
+                {
+                    return new DocumentDbStateSessionManagerWithTransactions(
+                        "StatefulServiceDemo",
+                        Guid.NewGuid(),
+                        "range-0",
+                        _settingsProvider
+                    );
+                }
+
+                [SetUp]
+                public override void Setup()
+                {
+                    _sessionId = Guid.NewGuid().ToString();
+                    var settingsProvider = CosmosDbForTestingSettingsProvider.DefaultForCollection(_sessionId);
+                    _settingsProvider = settingsProvider;
+                }
+
+                [TearDown]
+                public override void Teardown()
+                {
+                    if (_stateSessionManager is IDocumentDbDataManager dataManager)
+                        dataManager.DestroyCollecton(_settingsProvider.CollectionName).GetAwaiter().GetResult();
+                }
+
+                public IStateSessionManager CreateStateManager()
+                {
+                    _stateSessionManager = new DocumentDbStateSessionManagerWithTransactions(
+                        "StatefulServiceDemo",
+                        Guid.NewGuid(),
+                        "range-0",
+                        _settingsProvider
+                    );
+
+                    return _stateSessionManager;
+                }
             }
 
-            private void DestroyCollection()
+            public class Service_with_simple_queue_enqueued : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_simple_queue_enqueued
             {
-                if (_stateSessionManager is IDocumentDbDataManager dataManager)
-                    dataManager.DestroyCollecton(_collectionName).GetAwaiter().GetResult();
+                public Service_with_simple_queue_enqueued()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_simple_queue_enqueued.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
 
-            protected override void OnTearDown()
-            {
-                base.OnTearDown();
 
-                DestroyCollection();
+            public class Service_with_simple_dictionary : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_simple_dictionary
+            {
+                public Service_with_simple_dictionary()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_simple_dictionary.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
 
-            public TestRunnerBase(Func<StatefulServiceContext, IStateSessionManager, T> createService) : base(createService)
+            public class Service_with_multiple_states : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_multiple_states
             {
-            }
-        }
-
-        public class StateSession_transacted_scope : Runtime.StateSession_transacted_scope
-        {
-            private string _sessionId;
-            private CosmosDbForTestingSettingsProvider _settingsProvider;
-            private DocumentDbStateSessionManagerWithTransactions _stateSessionManager;
-
-            protected override IStateSessionManager GetStateSessionManager()
-            {
-                return new DocumentDbStateSessionManagerWithTransactions(
-                    "StatefulServiceDemo",
-                    Guid.NewGuid(),
-                    "range-0",
-                    _settingsProvider
-                );
+                public Service_with_multiple_states()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_multiple_states.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
 
-            [SetUp]
-            public override void Setup()
+            public class Service_with_simple_counter_state : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_simple_counter_state
             {
-                _sessionId = Guid.NewGuid().ToString();
-                var settingsProvider = CosmosDbForTestingSettingsProvider.DefaultForCollection(_sessionId);
-                _settingsProvider = settingsProvider;
+                public Service_with_simple_counter_state()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_simple_counter_state.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
 
-            [TearDown]
-            public override void Teardown()
+            public class Service_with_polymorphic_states : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_polymorphic_states
             {
-                if (_stateSessionManager is IDocumentDbDataManager dataManager)
-                    dataManager.DestroyCollecton(_settingsProvider.CollectionName).GetAwaiter().GetResult();
-            }
-
-            public IStateSessionManager CreateStateManager()
-            {
-                _stateSessionManager = new DocumentDbStateSessionManagerWithTransactions(
-                    "StatefulServiceDemo",
-                    Guid.NewGuid(),
-                    "range-0",
-                    _settingsProvider
-                );
-
-                return _stateSessionManager;
-            }
-        }
-
-        public class Service_with_simple_queue_enqueued : With_StateSession_All_Tests.
-            Service_with_simple_queue_enqueued
-        {
-            public Service_with_simple_queue_enqueued()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_simple_queue_enqueued.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-
-        public class Service_with_simple_dictionary : With_StateSession_All_Tests.
-            Service_with_simple_dictionary
-        {
-            public Service_with_simple_dictionary()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_simple_dictionary.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-        public class Service_with_multiple_states : With_StateSession_All_Tests.
-            Service_with_multiple_states
-        {
-            public Service_with_multiple_states()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_multiple_states.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-        public class Service_with_simple_counter_state : With_StateSession_All_Tests.
-            Service_with_simple_counter_state
-        {
-            public Service_with_simple_counter_state()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_simple_counter_state.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-        public class Service_with_polymorphic_states : With_StateSession_All_Tests.
-            Service_with_polymorphic_states
-        {
-            public Service_with_polymorphic_states()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_polymorphic_array_state.StatefulServiceDemo>(CreateService))
-            {
+                public Service_with_polymorphic_states()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_polymorphic_array_state.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
         }
     }
--- src/FG.ServiceFabric.Tests.Persistence/Services/Runtime/With_StateSession_InMemoryStateSession.cs
+++ src/FG.ServiceFabric.Tests.Persistence/Services/Runtime/With_StateSession_InMemoryStateSession.cs
@@ -9,86 +9,96 @@ using FG.ServiceFabric.Tests.StatefulServiceDemo;
 
 namespace FG.ServiceFabric.Tests.Persistence.Services.Runtime
 {
-    public class With_StateSession_InMemoryStateSession : With_StateSession_All_Tests
+    namespace With_StateSession_InMemoryStateSession
     {
-        private class TestRunnerBase<T> : TestRunnerWithFunc<T> where T : StatefulServiceDemoBase
+        public class With_StateSession_All_Tests : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+            With_StateSession_All_Tests
         {
-            private readonly IDictionary<string, string> _state = new ConcurrentDictionary<string, string>();
-
-            public TestRunnerBase(Func<StatefulServiceContext, IStateSessionManager, T> createServiceFunc) : base(createServiceFunc)
+            private class TestRunnerBase<T> : TestRunnerWithFunc<T> where T : StatefulServiceDemoBase
             {
+                private readonly IDictionary<string, string> _state = new ConcurrentDictionary<string, string>();
+
+                public TestRunnerBase(Func<StatefulServiceContext, IStateSessionManager, T> createServiceFunc) : base(
+                    createServiceFunc)
+                {
+                }
+
+                public override IDictionary<string, string> State => _state;
+
+                protected override void OnSetup()
+                {
+                    State.Clear();
+                    base.OnSetup();
+                }
+
+                public override IStateSessionManager CreateStateManager(MockFabricRuntime fabricRuntime,
+                    StatefulServiceContext context)
+                {
+                    return new InMemoryStateSessionManager(
+                        StateSessionHelper.GetServiceName(context.ServiceName),
+                        context.PartitionId,
+                        StateSessionHelper.GetPartitionInfo(context, () => fabricRuntime.PartitionEnumerationManager)
+                            .GetAwaiter()
+                            .GetResult(),
+                        _state);
+                }
             }
 
-            public override IDictionary<string, string> State => _state;
-
-            protected override void OnSetup()
+            public class Service_with_simple_queue_enqueued : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_simple_queue_enqueued
             {
-                State.Clear();
-                base.OnSetup();
+                public Service_with_simple_queue_enqueued()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_simple_queue_enqueued.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
 
-            public override IStateSessionManager CreateStateManager(MockFabricRuntime fabricRuntime,
-                StatefulServiceContext context)
+
+            public class Service_with_simple_dictionary : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_simple_dictionary
             {
-                return new InMemoryStateSessionManager(
-                    StateSessionHelper.GetServiceName(context.ServiceName),
-                    context.PartitionId,
-                    StateSessionHelper.GetPartitionInfo(context, () => fabricRuntime.PartitionEnumerationManager)
-                        .GetAwaiter()
-                        .GetResult(),
-                    _state);
+                public Service_with_simple_dictionary()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_simple_dictionary.StatefulServiceDemo>(CreateService))
+                {
+                }
+            }
+
+            public class Service_with_multiple_states : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_multiple_states
+            {
+                public Service_with_multiple_states()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_multiple_states.StatefulServiceDemo>(CreateService))
+                {
+                }
+            }
+
+            public class Service_with_simple_counter_state : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_simple_counter_state
+            {
+                public Service_with_simple_counter_state()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_simple_counter_state.StatefulServiceDemo>(CreateService))
+                {
+                }
+            }
+
+            public class Service_with_polymorphic_states : FG.ServiceFabric.Tests.Persistence.Services.Runtime.
+                With_StateSession_All_Tests.
+                Service_with_polymorphic_states
+            {
+                public Service_with_polymorphic_states()
+                    : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
+                        With_polymorphic_array_state.StatefulServiceDemo>(CreateService))
+                {
+                }
             }
         }
-
-        public class Service_with_simple_queue_enqueued : With_StateSession_All_Tests.
-            Service_with_simple_queue_enqueued
-        {
-            public Service_with_simple_queue_enqueued()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_simple_queue_enqueued.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-
-        public class Service_with_simple_dictionary : With_StateSession_All_Tests.
-            Service_with_simple_dictionary
-        {
-            public Service_with_simple_dictionary()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_simple_dictionary.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-        public class Service_with_multiple_states : With_StateSession_All_Tests.
-            Service_with_multiple_states
-        {
-            public Service_with_multiple_states()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_multiple_states.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-        public class Service_with_simple_counter_state : With_StateSession_All_Tests.
-            Service_with_simple_counter_state
-        {
-            public Service_with_simple_counter_state()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_simple_counter_state.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-
-        public class Service_with_polymorphic_states : With_StateSession_All_Tests.
-            Service_with_polymorphic_states
-        {
-            public Service_with_polymorphic_states()
-                : base(new TestRunnerBase<ServiceFabric.Tests.StatefulServiceDemo.
-                    With_polymorphic_array_state.StatefulServiceDemo>(CreateService))
-            {
-            }
-        }
-    }
+    }    
 }
