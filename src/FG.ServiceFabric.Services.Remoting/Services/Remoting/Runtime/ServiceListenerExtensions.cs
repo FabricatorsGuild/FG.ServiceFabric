70d7ca425704138cc203f56c28822ab93c4798b8
yoape <fredrik.goransson@gmail.com>
2018-01-31 13:19:08 +0100

    Add ServiceInstanceListener  extension to FG.ServiceFabric.Services.Remoting.Runtime

--- src/FG.ServiceFabric.Services.Remoting/Services/Remoting/Runtime/ServiceListenerExtensions.cs
+++ src/FG.ServiceFabric.Services.Remoting/Services/Remoting/Runtime/ServiceListenerExtensions.cs
@@ -8,9 +8,44 @@ using Microsoft.ServiceFabric.Services.Remoting.V1.FabricTransport.Runtime;
 
 namespace FG.ServiceFabric.Services.Remoting.Runtime
 {
+    /// <summary>
+    /// Extenstion methods to create service listeners with logging
+    /// </summary>
     public static class ServiceListenerExtensions
     {
-        public static ServiceReplicaListener CreateServiceReplicaListener(this IService service, ServiceContext context,
+        /// <summary>
+        /// Creates a ServiceInstanceListener for a stateless service with embedded logging using the supplied IServiceCommunicationLogger
+        /// </summary>
+        /// <param name="service">The owning service exposing the listener</param>
+        /// <param name="context">The context of the service</param>
+        /// <param name="logger">The IServiceCommunicationLogger logging service requests and failures</param>
+        /// <returns></returns>
+        public static ServiceInstanceListener CreateServiceReplicaListener(this IService service, StatelessServiceContext context,
+            IServiceCommunicationLogger logger)
+        {
+            return new ServiceInstanceListener(ctxt =>
+                (IServiceRemotingListener)new FabricTransportServiceRemotingListener(
+                    ctxt,
+                    new ServiceRemotingDispatcher(
+                        service,
+                        new Microsoft.ServiceFabric.Services.Remoting.V1.Runtime.ServiceRemotingDispatcher(context,
+                            service),
+                        logger),
+                    new FabricTransportRemotingListenerSettings
+                    {
+                        MaxConcurrentCalls = 1000
+                    }
+                ));
+        }
+
+        /// <summary>
+        /// Creates a ServiceReplicaListener for a stateful service with embedded logging using the supplied IServiceCommunicationLogger
+        /// </summary>
+        /// <param name="service">The owning service exposing the listener</param>
+        /// <param name="context">The context of the service</param>
+        /// <param name="logger">The IServiceCommunicationLogger logging service requests and failures</param>
+        /// <returns></returns>
+        public static ServiceReplicaListener CreateServiceReplicaListener(this IService service, StatefulServiceContext context,
             IServiceCommunicationLogger logger)
         {
             return new ServiceReplicaListener(ctxt =>
