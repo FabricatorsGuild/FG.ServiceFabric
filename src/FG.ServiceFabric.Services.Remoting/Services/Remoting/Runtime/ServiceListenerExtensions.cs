b23c07c2750afa61b1b023d5448c2bb70693368b
yoape <fredrik.goransson@gmail.com>
2018-02-02 12:00:15 +0100

    add settings to FabricTransportServiceRemotingListener for service listener extensions

--- src/FG.ServiceFabric.Services.Remoting/Services/Remoting/Runtime/ServiceListenerExtensions.cs
+++ src/FG.ServiceFabric.Services.Remoting/Services/Remoting/Runtime/ServiceListenerExtensions.cs
@@ -14,27 +14,33 @@ namespace FG.ServiceFabric.Services.Remoting.Runtime
     public static class ServiceListenerExtensions
     {
         /// <summary>
+        /// Gets the default settings for FabricTransportRemotingListenerSettings
+        /// </summary>
+        public static FabricTransportRemotingListenerSettings DefaultListenerSettings =>
+            new FabricTransportRemotingListenerSettings()
+            {
+                MaxConcurrentCalls = 1000,
+            };
+
+        /// <summary>
         /// Creates a ServiceInstanceListener for a stateless service with embedded logging using the supplied IServiceCommunicationLogger
         /// </summary>
         /// <param name="service">The owning service exposing the listener</param>
         /// <param name="context">The context of the service</param>
         /// <param name="logger">The IServiceCommunicationLogger logging service requests and failures</param>
+        /// <param name="settings">Settings for the communication. Note, all services in the same project (process) MUST share the same settings</param>
         /// <returns></returns>
         public static ServiceInstanceListener CreateServiceReplicaListener(this IService service, StatelessServiceContext context,
-            IServiceCommunicationLogger logger)
+            IServiceCommunicationLogger logger, FabricTransportRemotingListenerSettings settings = null)
         {
             return new ServiceInstanceListener(ctxt =>
                 (IServiceRemotingListener)new FabricTransportServiceRemotingListener(
                     ctxt,
                     new ServiceRemotingDispatcher(
                         service,
-                        new Microsoft.ServiceFabric.Services.Remoting.V1.Runtime.ServiceRemotingDispatcher(context,
-                            service),
+                        new Microsoft.ServiceFabric.Services.Remoting.V1.Runtime.ServiceRemotingDispatcher(context, service),
                         logger),
-                    new FabricTransportRemotingListenerSettings
-                    {
-                        MaxConcurrentCalls = 1000
-                    }
+                    settings ?? DefaultListenerSettings
                 ));
         }
 
@@ -46,20 +52,16 @@ namespace FG.ServiceFabric.Services.Remoting.Runtime
         /// <param name="logger">The IServiceCommunicationLogger logging service requests and failures</param>
         /// <returns></returns>
         public static ServiceReplicaListener CreateServiceReplicaListener(this IService service, StatefulServiceContext context,
-            IServiceCommunicationLogger logger)
+            IServiceCommunicationLogger logger, FabricTransportRemotingListenerSettings settings = null)
         {
             return new ServiceReplicaListener(ctxt =>
                 (IServiceRemotingListener) new FabricTransportServiceRemotingListener(
                     ctxt,
                     new ServiceRemotingDispatcher(
                         service,
-                        new Microsoft.ServiceFabric.Services.Remoting.V1.Runtime.ServiceRemotingDispatcher(context,
-                            service),
+                        new Microsoft.ServiceFabric.Services.Remoting.V1.Runtime.ServiceRemotingDispatcher(context, service),
                         logger),
-                    new FabricTransportRemotingListenerSettings
-                    {
-                        MaxConcurrentCalls = 1000
-                    }
+                    settings ?? DefaultListenerSettings
                 ));
         }
     }
